e4fb8e0978eb113f5a54d4c973b52f4342b62eb2
kaif Ali <kaifali.6263@gmail.com>
2026-09-05 20:52:09 +0530

    Time: 11 ms (57.63%), Space: 210.7 MB (56.49%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+         int n = nums.size();
+
+        vector<int> maxprefix(n);
+        vector<int> minprefix(n);
+
+        // Prefix maximum
+        maxprefix[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            maxprefix[i] = max(maxprefix[i - 1], nums[i]);
+        }
+
+        // Suffix minimum
+        minprefix[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            minprefix[i] = min(minprefix[i + 1], nums[i]);
+        }
+
+        // Find first stable index
+        for (int i = 0; i < n; i++) {
+            if (maxprefix[i] - minprefix[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
